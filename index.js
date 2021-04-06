52f0e17e53c0592cd60d103ffc2876f0073aed23
cbarange <cbarange.dev@gmail.com>
2021-04-06 09:28:24 +0200

    US3 - Affiche la tva en fonction du pays

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ const question = question_text =>  new Promise((resolve, reject) => rl.question(
 
 const tva_by_countrycode = [{code:'BE', value:21},{code:'FR', value:20},{code:'US', value:8.75},{code:'CH', value:7.7},{code:'UK', value:17.5},{code:'ES', value:21}]
 
-
 async function main(){
   let state_code
   do{
-    await question(`Veuillez saisir le code d'état ? ${tva_by_countrycode.map(e=>e.code).join`/`}`).then(response=>state_code=response)
-  }while(!tva_by_countrycode.map(e=>e.code.toLowerCase()).includes(state_code.toLowerCase().trim()))
+    state_code = await question(`Veuillez saisir le code d'état ? ${tva_by_countrycode.map(e=>e.code).join`/`}`).then(response=>tva_by_countrycode.reduce((r,e)=>e.code.toLowerCase()==response.toLowerCase().trim()?r=e:r=r,undefined))
+  }while(!state_code || !tva_by_countrycode.map(e=>e.code.toLowerCase()).includes(state_code.code.toLowerCase().trim()))
 
-  console.log("Le code pays choisi est :")
-  console.log(state_code)
+  console.log("La tva du pays choisi est :")
+  console.log(state_code.value)
 
   rl.close()
 }
